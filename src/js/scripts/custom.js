a4f789e5f5e3383dee7e94bd3947431716a3732c
Polovkin <zimboroda@gmail.com>
2020-08-19 09:13:15 +0300

    done

--- src/js/scripts/custom.js
+++ src/js/scripts/custom.js
@@ -1,116 +1,135 @@
 (function () {
-    function sendFormDataXMLHttp(form) {
-        let request = new XMLHttpRequest();
-        request.open('POST', '/form/send.php', true)
-        request.setRequestHeader('accept', 'application/json');
+        function sendFormDataXMLHttp(form) {
+            let request = new XMLHttpRequest();
+            request.open('POST', '/form/send.php', true)
+            request.setRequestHeader('accept', 'application/json');
 
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            if (validNumber && validCVV && validDate) {
-                let formData = new FormData(form);
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
 
-                request.send(formData);
+                if (validNumber && validCVV && validDate) {
+                    let formData = new FormData(form);
 
-                request.onreadystatechange = function () {
-                    if (request.readyState < 4)
-                        console.log('Ответ от сервера полностью загружен')
-                    else if (request.readyState === 4) {
-                        if (request.status === 200 && request.status < 300) {
-                            console.log('200 - 299 = успешная отправка данных!')
-                            window.location.href = `http://misha.amemory.pro/`;
-                        } else {
-                            console.log('что-то пошло не так')
+                    request.send(formData);
+
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4)
+                            console.log('Ответ от сервера полностью загружен')
+                        else if (request.readyState === 4) {
+                            if (request.status === 200 && request.status < 300) {
+                                console.log('200 - 299 = успешная отправка данных!')
+                                window.location.href = `http://misha.amemory.pro/`;
+                            } else {
+                                console.log('что-то пошло не так')
+                            }
                         }
                     }
+                } else {
+                    console.log('fail')
                 }
-            } else {
-                console.log('fail')
-            }
-        });
-    }
-
-    function luhnAlgorithm(digits) {
-        let sum = 0;
-
-        for (let i = 0; i < digits.length; i++) {
-            let cardNum = parseInt(digits[i]);
-
-            if ((digits.length - i) % 2 === 0) {
-                cardNum = cardNum * 2;
-
-                if (cardNum > 9) {
-                    cardNum = cardNum - 9;
-                }
-            }
-
-            sum += cardNum;
+            });
         }
 
-        return sum % 10 === 0;
-    }
+        function luhnAlgorithm(digits) {
+            let sum = 0;
 
-    function onlyNumber(input) {
-        input.value = input.value.replace(/[^\d]/g, '');
-    }
+            for (let i = 0; i < digits.length; i++) {
+                let cardNum = parseInt(digits[i]);
 
+                if ((digits.length - i) % 2 === 0) {
+                    cardNum = cardNum * 2;
 
+                    if (cardNum > 9) {
+                        cardNum = cardNum - 9;
+                    }
+                }
 
+                sum += cardNum;
+            }
 
+            return sum % 10 === 0;
+        }
 
-    let validNumber = false;
-    let validDate = false;
-    let validCVV = false;
-    const forms = document.querySelectorAll('form')
+        function onlyNumber(input) {
+            input.value = input.value.replace(/[^\d]/g, '');
+        }
 
-    console.log(checkDate(2000, 11));
+        function checkData(date) {
+            let d = new Date();
+            let currentYear = d.getFullYear();
+            let currentMonth = d.getMonth() + 1;
+            let parts = date.split('/');
+            let year = parseInt(parts[1], 10) + 2000;
+            let month = parseInt(parts[0], 10);
 
-    if (forms.length) {
-        for (let i = 0; i < forms.length; i++) {
-            let label = forms[i].querySelectorAll('label')
+            return (year < currentYear || (year === currentYear && month < currentMonth))
+        }
 
-            for (let i = 0; i < label.length; i++) {
-                const errorMsg = label[i].querySelector('.error')
-                const input = label[i].querySelector('input')
-                if (input) {
-                    input.addEventListener('invalid', function (event) {
-                        event.preventDefault();
-                        if (!event.target.validity.valid) {
-                            errorMsg.style.opacity = '1';
-                            setTimeout(function () {
-                                errorMsg.style.opacity = '0';
-                            }, 3000)
-                        } else {
+        const form = document.querySelector('form')
+        const inputNumber = form.querySelector('input[name="cardNumber"]')
+        const inputDate = form.querySelector('input[name="cardDate"]')
+        let label = form.querySelectorAll('label')
+
+        for (let i = 0; i < label.length; i++) {
+            const errorMsg = label[i].querySelector('.error')
+            const input = label[i].querySelector('input')
+            if (input) {
+                input.addEventListener('invalid', function (event) {
+                    event.preventDefault();
+                    if (!event.target.validity.valid) {
+                        errorMsg.style.opacity = '1';
+                        setTimeout(function () {
                             errorMsg.style.opacity = '0';
-                        }
-                    })
-                }
-                if (input.name === 'name') {
-                    input.addEventListener('keyup', (e) => {
-                        onlyNumber(input)
-
-                        let value = input.value.split(" ").join("");
-                        if (value.length > 0) {
-                            value = value.match(new RegExp('.{1,4}', 'g')).join(" ");
-                        }
-                        input.value = value
-                        validNumber = luhnAlgorithm(input.value.replace(/\s/g, ''))
-                    })
-                }
-                if (input.name === 'cardDate') {
-                    input.addEventListener('keyup', (e) => {
-                        onlyNumber(input);
-                        let value = input.value.split(" ").join("");
-                        if (value.length > 0) {
-                            value = value.match(new RegExp('.{1,2}', 'g')).join("/");
-                        }
-                        input.value = value
-
-                        validDate = luhnAlgorithm(input.value.replace(/\s/g, ''))
-                    })
-                }
+                        }, 3000)
+                    } else {
+                        errorMsg.style.opacity = '0';
+                    }
+                })
             }
-            sendFormDataXMLHttp(forms[i])
-
         }
+
+        inputNumber.addEventListener('keyup', (e) => {
+            onlyNumber(inputNumber)
+
+            let value = inputNumber.value.split(" ").join("");
+            if (value.length > 0) {
+                value = value.match(new RegExp('.{1,4}', 'g')).join(" ");
+            }
+            inputNumber.value = value
+
+        })
+
+        inputDate.addEventListener('keyup', (e) => {
+            onlyNumber(inputDate);
+            let value = inputDate.value.split(" ").join("");
+            if (value.length > 0) {
+                value = value.match(new RegExp('.{1,2}', 'g')).join("/");
+            }
+            inputDate.value = value;
+        })
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let validDate = false;
+            let validNumber = false;
+            if (!luhnAlgorithm(inputNumber.value.replace(/\s/g, ''))) {
+                inputNumber.classList.add('invalid')
+            }
+            if (checkData(inputDate.value)) {
+                inputDate.classList.add('invalid')
+            }
+            if (validDate && validNumber) {
+                let inputs = document.querySelectorAll('input');
+                for (let j = 0; j < inputs.length; j++) {
+                    inputs[j].classList.remove('invalid')
+                }
+                alert('valid')
+                //send dta
+            }
+        })
+
+
     }
-}());
+    ()
+);
+//5168 7450 1005 4622
